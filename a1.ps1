686bd8da73791b0ddfbc7a636a98a5d522edff61
adishiritwick <91323393+adishiritwick@users.noreply.github.com>
2021-10-26 21:44:04 +0530

    Update a1.ps1

--- a1.ps1
+++ a1.ps1
@@ -12,4 +12,3 @@ $resource= "?resource=https://management.azure.com/"
 $url = $env:IDENTITY_ENDPOINT + $resource 
 $Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]" 
 $Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
-$Headers.Add("Metadata", "True") 
