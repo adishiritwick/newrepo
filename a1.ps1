a2ad30ec365ec88e767a4d51228f85083f60ba16
adishiritwick <91323393+adishiritwick@users.noreply.github.com>
2021-10-26 21:59:11 +0530

    Update a1.ps1

--- a1.ps1
+++ a1.ps1
@@ -14,6 +14,4 @@ $Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
 $Headers.Add("Metadata", "True") 
 $accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
-Write-Output $accessToken.access_token
-
 
