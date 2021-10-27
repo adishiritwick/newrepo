e13fdfada672e87306a3ed9ac87fef27d3af1cdc
adishiritwick <91323393+adishiritwick@users.noreply.github.com>
2021-10-27 23:54:23 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -7,6 +7,4 @@ $accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
 Write-Output $accessToken.access_token
 
 $resource= "?resource=https://management.azure.com/" 
-$url = $env:IDENTITY_ENDPOINT + $resource 
-$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]" 
-$Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
+
